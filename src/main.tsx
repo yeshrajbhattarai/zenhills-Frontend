26dad655568bfae2b356a1533f1362381a683374
YeshBhattz <86815567+YeshBhattz@users.noreply.github.com>
2026-05-12 21:54:07 +0530

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-// import { createRoot } from "react-dom/client";
-// import App from "./App.tsx";
-// import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-// createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);
 
-if (window.location.pathname !== "/maintenance.html") {
-  window.location.replace("/maintenance.html");
-}
+// if (window.location.pathname !== "/maintenance.html") {
+//   window.location.replace("/maintenance.html");
+// }
